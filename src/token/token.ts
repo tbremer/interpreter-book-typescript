c4f29b7442dbc56a2ec55e4cca2e3d9747681edf
Tom Bremer <tom@tbremer.com>
2020-12-21 19:33:01 -0600

    Extend lexer for mathematical symbols

--- src/token/token.ts
+++ src/token/token.ts
@@ -8,8 +8,17 @@ export enum TokenType {
   EOF = "EOF",
   Ident = "Ident", // x, y, foo, bar
   Int = "Int", // 1,2,44
+  
+  // Operators
   Assign = "Assign", // =
   Plus = "Plus",
+  Minus = "Minus",
+  Bang = "Bang",
+  Asterisk = "Asterisk",
+  Slash = "Slash",
+  LessThan = "LessThan",
+  GreaterThan = "GreaterTahn",
+
   Comma = "Comma",
   Semicolon = "Semicolon",
   LParen = "LParen",
